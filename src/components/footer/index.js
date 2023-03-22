3fbbe90ace6ac43ba192317499fa3c74276c5fdd
tyrkgitgub <tiayorke@hotmail.co.uk>
2023-03-22 19:24:28 +0000

    added links to our githubs in footer and began work on the hero

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,17 +1,57 @@
 // importing various folders/libraries to use here
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 import "./style.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons"
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 //function that creates a footer element for the App
 function Footer() {
-    return (
-        <footer className="section">
-            <h4>This was created by Sasha Zhyliuk, Tia Yorke, Niwantha Meepage, and Rebecca Wood <FontAwesomeIcon icon={faHeart} /></h4>
-        </footer>
-    );
-};
+  return (
+    <footer className="section">
+      <div className="container-fluid justify-content-center">
+        <ul className="navbar-nav justify-content-center">
+          <li className="nav-item">
+            <a
+              target="_blank"
+              className="footerLinks"
+              href="https://github.com/ResonantDevotion"
+            >
+              Rebecca <FaGithub />
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              target="_blank"
+              className="footerLinks"
+              href="https://github.com/tyrkgithub"
+            >
+              Tia <FaGithub />
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              target="_blank"
+              className="footerLinks"
+              href="https://github.com/SashK2609"
+            >
+              Oleksandra <FaGithub />
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              target="_blank"
+              className="footerLinks"
+              href="https://github.com/niwantha33"
+            >
+              Niwantha <FaGithub />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
 //exports the file to be used elsewhere
 export default Footer;
