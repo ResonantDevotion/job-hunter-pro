ed356720813fd264eeb72d0f8bfe33fde832d019
SashaKorobkina <s.dekaiper@gmail.com>
2023-03-22 19:34:01 +0000

    installed axios and node-schedule npm package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 
+
 import Frontpage from './pages/frontpage/frontpage';
 
 function App() {
--- src/components/jobSearch/index.js
+++ src/components/jobSearch/index.js
@@ -1,6 +1,8 @@
 // importing various folders/libraries to use here
 import React from "react";
 import "./style.css";
+import axios from "axios";
+import schedule from "node-schedule";
 
 //function that creates a job search component for the App
 function JobSearch() {
