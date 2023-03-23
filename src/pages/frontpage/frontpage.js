499ee04f35e3c7062a0140fc5c8afe96c1b29cd6
rebecca wood <rebeccaseverowood@gmail.com>
2023-03-24 07:27:57 +1300

    added local storage for user profile input and functional buttons

--- src/pages/frontpage/frontpage.js
+++ src/pages/frontpage/frontpage.js
@@ -10,7 +10,6 @@ import Footer from "../../components/footer";
 // function that creates a body for the homepage including information about what the app is about
 function Frontpage() {
 
-
     return (
         <section className="body section">
             <Navbar />
