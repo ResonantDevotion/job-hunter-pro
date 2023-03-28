87a46c1878b48ead7b3a83cb6bad43a2ae167c34
tyrkgitgub <tiayorke@hotmail.co.uk>
2023-03-28 17:33:28 +0100

    more bug fixes

--- src/components/cards/profileCard/profileCard.js
+++ src/components/cards/profileCard/profileCard.js
@@ -12,19 +12,11 @@ const ProfileCard = (props) => {
       <h2 className="banner text-center">Your Profile</h2>
       <div className="container-fluid d-flex justify-content-center">
         <div className="profileCard text-center justify-content-center">
-<<<<<<< HEAD
           <div className="profileCard-body justify-content-center">
             <h4 className="profileCard-title">{userInfo[0].userName}</h4>
             <p className="location text-center">{userInfo[0].userLocation}</p>
             <p className="experience text-center">{userInfo[0].userExperience}</p>
             <p className="skills text-center">{userInfo[0].userSkills}</p>
-=======
-          <div className="profileCard-body">
-            <h4 className="profileCard-title">{userInfo[0].userName}</h4>
-            <p className="card-text text-secondary">{userInfo[0].userLocation}</p>
-            <p className="card-text text-secondary">{userInfo[0].userExperience}</p>
-            <ul className="skills text-center">{userInfo[0].userSkills}</ul>
->>>>>>> main
           </div>
         </div>
       </div>
