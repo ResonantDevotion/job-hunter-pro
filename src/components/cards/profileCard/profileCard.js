e88072adde2708e184feaed606c8453e070e748a
SashaKorobkina <s.dekaiper@gmail.com>
2023-03-30 16:24:33 +0100

    localStorage fixes + Clear Card button fix + added build script

--- src/components/cards/profileCard/profileCard.js
+++ src/components/cards/profileCard/profileCard.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./profileCard.css";
 
 // retrieving stored infomration in local storage
-const userInfo = JSON.parse(localStorage.getItem("userInfo")) || []
+const userInfo = JSON.parse(localStorage.getItem("userInfo") || '[]');
 
 // function to create the profile card with the retrieved LS data.
 function ProfileCard() {
@@ -13,10 +13,10 @@ function ProfileCard() {
       <div className="container-fluid d-flex justify-content-center">
         <div className="profileCard text-center justify-content-center">
           <div className="profileCard-body justify-content-center">
-            <h4 className="profileCard-title">{userInfo[0].userName}</h4>
-            <p className="location text-center">{userInfo[0].userLocation}</p>
-            <p className="experience text-center">{userInfo[0].userExperience}</p>
-            <p className="skills text-center">{userInfo[0].userSkills}</p>
+            <h4 className="profileCard-title">{userInfo[0] && userInfo[0].userName}</h4>
+            <p className="location text-center">{userInfo[0] && userInfo[0].userLocation}</p>
+            <p className="experience text-center">{userInfo[0] && userInfo[0].userExperience}</p>
+            <p className="skills text-center">{userInfo[0] && userInfo[0].userSkills}</p>
           </div>
         </div>
       </div>
